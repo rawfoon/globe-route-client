e02a693d12ba07894398c5a962835f27add14cb1
rawfoon <mgrawfoon@gmail.com>
2022-11-10 23:23:12 +0600

    private route complete and loader integreated

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -5,7 +5,23 @@ import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const PrivateRoute = ({children}) => {
 
-    
+    const {user, loading} = useContext(AuthContext)
+    const location = useLocation()
+
+    if(loading){
+        return <>
+        <div className=" flex justify-center items-center flex-col">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+          <div><p className="mt-4 animate-bounce">Loading...</p></div>
+        </div>
+        </>
+    }
+
+    if(!user){
+        return <Navigate to='/login' state={{from: location}} replace></Navigate>
+    }
+
+    return children 
 };
 
 export default PrivateRoute;
