210ef76601913db3e6f5872b0debc10c0b1a3ecf
rawfoon <mgrawfoon@gmail.com>
2022-11-10 23:03:22 +0600

    google sign in integreated

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -112,26 +112,26 @@ const Login = () => {
         const user = result.user
         // console.log(user)
 
-        const currentUser = {
-          email: user.email
-        }
+        // const currentUser = {
+        //   email: user.email
+        // }
 
-        // get jwt 
+        // // get jwt 
 
-        fetch(`https://globe-route-travels.vercel.app/jwt`,{
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json'
-          },
-          body: JSON.stringify(currentUser)
-        })
-        .then(res => res.json())
-        .then(data=>{
-          // console.log(data);
-          localStorage.setItem('globeRoutTravelToken', data.token)
-          navigate(from, {replace: true})
-        })
-        .catch(e => console.error(e))
+        // fetch(`https://globe-route-travels.vercel.app/jwt`,{
+        //   method: 'POST',
+        //   headers: {
+        //     'content-type': 'application/json'
+        //   },
+        //   body: JSON.stringify(currentUser)
+        // })
+        // .then(res => res.json())
+        // .then(data=>{
+        //   // console.log(data);
+        //   localStorage.setItem('globeRoutTravelToken', data.token)
+        //   navigate(from, {replace: true})
+        // })
+        // .catch(e => console.error(e))
         
     })
     .catch(error => console.error(error))
