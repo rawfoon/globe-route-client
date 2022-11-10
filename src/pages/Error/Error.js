797b4c195e765b18fcd9c649544031bf83ea4118
rawfoon <mgrawfoon@gmail.com>
2022-11-10 23:16:03 +0600

    my review page created

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -12,7 +12,7 @@ const Error = () => {
 			</h2>
 			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
 			<p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-			<Link to='/' rel="noopener noreferrer"  className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
+			<Link to='/'   className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
 		</div>
 	</div>
 </section>
